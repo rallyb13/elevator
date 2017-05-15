627cf97bc83da52981fbdb13a8c0c72ec15e7d52
Benjamin Herson <benjaminherson@gmail.com>
2017-05-15 15:48:35 -0700

    add call panel and connect to summonElevator action, add handleTime && handleMotion functions to allow elevators to move

--- app/components/waiting-area.js
+++ app/components/waiting-area.js
@@ -5,5 +5,23 @@ export default Ember.Component.extend({
   elevatorService: Ember.inject.service('elevator-control'),
 
   // Properties
-  activeFloor: 1
+  activeFloor: 2,
+  hasStarted: false,
+
+  // Hooks
+  didRender() {
+    if (this.get('hasStarted') === false) {
+      this.get('elevatorService').handleTime();
+      this.set('hasStarted', true); // TODO: elim hasStarted && check if poss
+    }
+  },
+
+  // Actions
+  actions: {
+    callElevator(isGoingUp) {
+      const elevatorServ = this.get('elevatorService');
+      elevatorServ.summonElevator(this.activeFloor, isGoingUp);
+      // TODO: add UI for showing button is pressed && logic: if it's pressed don't call again
+    }
+  }
 });
--- app/services/elevator-control.js
+++ app/services/elevator-control.js
@@ -4,30 +4,38 @@ const {ActionHandler} = Ember;
 export default Ember.Service.extend(ActionHandler, {
   // State
   isPersOnBoard: false, // Pers = person/perspective
+  isSystemActive: true,
+  timerId: 0,
   elevators: [
     {
       isDoorOpen: false,
+      doorCount: 3,
       isInTransit: true,
       isGoingUp: false,
       currentFloor: 7,
       destinationFloor: 2,
-      stops: [4]
+      stops: [5],
+      track: 0
     },
     {
-      isDoorOpen: true,
+      isDoorOpen: false,
+      doorCount: 2,
       isInTransit: false,
       isGoingUp: false,
       currentFloor: 1,
       destinationFloor: 1,
-      stops: []
+      stops: [],
+      track: 1
     },
     {
       isDoorOpen: false,
+      doorCount: 3,
       isInTransit: true,
       isGoingUp: true,
       currentFloor: 3,
       destinationFloor: 18,
-      stops: [9, 4, 14]
+      stops: [9, 4, 14],
+      track: 2
     }
   ],
 
@@ -38,7 +46,6 @@ export default Ember.Service.extend(ActionHandler, {
       bestScore = 100,
       selectedEl = {};
 
-
     for (let i=0; i<this.elevators.length; i++) {
       const current = this.elevators[i].currentFloor,
         dest = this.elevators[i].destinationFloor;
@@ -46,7 +53,7 @@ export default Ember.Service.extend(ActionHandler, {
       if (this.elevators[i].isInTransit === false) {
         // if elevator not in use
         score = Math.abs(current - floor);
-      } else if (this.elevators[i].isGoingUp !== isUp) {
+      } else if (this.elevators[i].isGoingUp !== isUp) { // TODO: add count of stops to score
         // if elevator is heading opposite direction TODO: flag to skip floor if pass before changing direction?
         score = Math.abs(current - dest);
         score += Math.abs(dest - floor);
@@ -73,24 +80,91 @@ export default Ember.Service.extend(ActionHandler, {
 
     selectedEl = this.elevators[index];
     // now add assignment into elevator data
-    // TODO: check if doors open when on current floor once adding info in, or if just miss
+    // TODO: check if doors can open first when on current floor once adding info in, or if just miss (becomes off by one error)
     if (selectedEl.isInTransit === false){
-      selectedEl.isInTransit = true;
-      selectedEl.isGoingUp = isUp;
-    }
-
-    if (selectedEl.destinationFloor !== floor) {
+      Ember.set(selectedEl, 'isInTransit', true);
+      Ember.set(selectedEl, 'isGoingUp', isUp);
+      Ember.set(selectedEl, 'destinationFloor', floor);
+    } else if (selectedEl.destinationFloor !== floor) {
       if (isUp === floor < selectedEl.destinationFloor) {
-        selectedEl.stops.push(floor);
+        selectedEl.stops.pushObject(floor);
       } else {
-        selectedEl.stops.push(selectedEl.destinationFloor);
-        selectedEl.destinationFloor = floor;
+        selectedEl.stops.pushObject(selectedEl.destinationFloor);
+        Ember.set(selectedEl, 'destinationFloor', floor);
       }
     }
+    this.handleMotion();
+  },
+
+  handleTime() {
+    const me = this;
+    let timeoutId = this.get('timerId');
+    window.clearTimeout(timeoutId);
+    if (this.get('isSystemActive') === true) {
+      timeoutId = window.setTimeout(function () {
+        me.handleMotion();
+      }, 1000);
+      this.set('timerId', timeoutId);
+    }
+  },
+
+  handleMotion() {
+    const movingEls = this.elevators.filter(obj => obj.isInTransit === true);
+    let change = 1,
+      newStops = [],
+      newDest = 0;
+
+    this.set('isSystemActive', true);
+    for (let i=0; i<movingEls.length; i++) {
+      let elev = movingEls[i],
+        elData = this.elevators[elev.track];
+
+      if (elev.isDoorOpen === true) {
+        // Hodor
+        if (elev.doorCount > 0) {
+          Ember.set(elData, 'doorCount', elev.doorCount - 1);
+        } else {
+          Ember.set(elData, 'isDoorOpen', false);
+          Ember.set(elData, 'doorCount', 3); // reset door timer
+        }
+      } else {
+        if (elev.destinationFloor === elev.currentFloor) {
+          // reaches destination
+          Ember.set(elData, 'isDoorOpen', true);
+          if (elev.stops.length === 0) {
+            Ember.set(elData, 'isInTransit', false);
+          } else {
+            newStops = elev.stops; // TODO: sort first so that additional calls don't get confused over destination (not important for current-possible UX)
+            newDest = newStops.shift();
+            Ember.set(elData, 'destinationFloor', newDest);
+            Ember.set(elData, 'stops', newStops);
+            Ember.set(elData, 'isGoingUp', newDest > elev.currentFloor);
+          }
+        } else if (elev.stops.indexOf(elev.currentFloor) !== -1) {
+          // open doors at a stop
+          Ember.set(elData, 'isDoorOpen', true);
+          newStops = elev.stops
+          newStops.splice(elev.stops.indexOf(elev.currentFloor), 1);
+          Ember.set(elData, 'stops', newStops);
+        } else {
+          // or move elevator
+          change = elev.isGoingUp ? 1 : -1;
+          Ember.set(elData, 'currentFloor', elev.currentFloor + change);
+        }
+      }
+    }
+    if (movingEls.length === 0) {
+      this.set('isSystemActive', false);
+    } else {
+      this.handleTime();
+    }
+
+
+
   }
 
   /*TODO: write more functionality:
   * add destination (in elevator)
-  * check for stops, as part of handle doors & motion [this function is called by time to refresh data]
+  * check for stops, as part of handle doors & motion [this function is called by time interval to refresh data]
   */
 });
